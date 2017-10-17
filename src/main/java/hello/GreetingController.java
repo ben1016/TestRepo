d609635bcc7f5d082c04454f10ecafa2e471995e
ben1016 <ben.ng.work@gmail.com>
2017-10-17 21:34:24 +0800

    First commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s, %s, %s, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name,
+    		@RequestParam(value="name2", defaultValue="World2") String name2,
+    		@RequestParam(value="name3", defaultValue="World3") String name3,
+    		@RequestParam(value="name4", defaultValue="World4") String name4) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name, name2, name3, name4));
+    }
+}
